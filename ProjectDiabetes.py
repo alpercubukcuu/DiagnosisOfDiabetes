8101d20bf54a932f9b781df096d13733c9b7074e
alpercubukcu <alpercubukcuu@gmail.com>
2023-03-23 14:40:19 +0300

    Fix

--- ProjectDiabetes.py
+++ ProjectDiabetes.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
 
 #Outcome = 1 Diyebet/Şeker Hastası
 #Outcome = 0 Sağlıklı
@@ -27,6 +28,8 @@ x_data = data.drop(['Outcome'], axis=1)
 #Outcome sütünunu(depent variable) çıkarıp sadece independent variables bırakıyoruz
 # Çünkü KNN algoritması x degerleri içerisinde gruplandırma yapacak..
 
+
+
 # Normalization yapıyoruz - x_data içerisindeki degerler sadece 0 ve 1 arasında olacak şekilde hepsini güncelliyoruz
 # Eğer bu şekilde normalization yapmazsak yüksek rakam küşük rakamları ezer ve KNN algoritması yanılta bilir!
 
@@ -46,7 +49,14 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.01, random
 knn = KNeighborsClassifier(n_neighbors=4) # n_neighbors = k
 knn.fit(x_train, y_train)
 prediction = knn.predict(x_test)
-print('K=3 için test verilerimizin doğrulama testi sonucu', knn.score(x_test, y_test))
+print('K=4 için test verilerimizin doğrulama testi sonucu', knn.score(x_test, y_test))
+
+# normalization yapıyoruz - daha hızlı normalization yapabilmek için MinMax  scaler kullandık...
+sc = MinMaxScaler()
+sc.fit_transform(x_data)
+
+new_prediction = knn.predict(sc.transform(np.array([[6, 148, 72, 35, 0, 33.6, 0.627, 50]])))
+new_prediction[0]
 
 # k kaç olmalı ?
 # en iyi k degerini belirleyelim..
@@ -57,3 +67,4 @@ for k in range(1, 11):
     knn_new.fit(x_train, y_train)
     print(count, '', 'Doğruluk oranı: %', knn_new.score(x_test, y_test)*100)
     count += 1
+
